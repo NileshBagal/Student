8aaea718309c030576774796e9fffc3890251589
Nilesh <n2bagal@gmail.com>
2019-09-09 18:18:44 +0530

    first commit

---
+++ firstapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+'''class tutorial(models.Model):
+    tutorial_name = models.CharField(max_length=200)
+    tutorial_roll=models.IntegerField()
+    tutorial_DOB=models.DateField("Date of Birth")
+
+    def __str__(self):
+        return self.tutorial_name'''
+
